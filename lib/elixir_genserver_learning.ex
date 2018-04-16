a2498e98fbf58ffa4a42992c38bb25140cef66e2
HoffsMH <hecker.matthew@gmail.com>
2018-04-16 10:18:25 -0400

    initial commit

---
+++ lib/elixir_genserver_learning.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGenserverLearning do
+  @moduledoc """
+  Documentation for ElixirGenserverLearning.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGenserverLearning.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_genserver_learning_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirGenserverLearningTest do
+  use ExUnit.Case
+  doctest ElixirGenserverLearning
+
+  test "greets the world" do
+    assert ElixirGenserverLearning.hello() == :world
+  end
+end
